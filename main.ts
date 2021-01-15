065f17b6f9d4de03c36531622b05cd8f2c5c0a60
msgumtow <71787320+msgumtow@users.noreply.github.com>
2021-01-15 14:20:39 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+/**
+ * Put together the puzzle! We want small and large diamond icons to animate when the sound level is above a certain number. When the level is below that number, we want the microbit to be blank. 
+ * 
+ * Forever
+ * 
+ * While the sound level is greater than/equal to 100, do show icon (big diamond) and show icon (little diamond).
+ * 
+ * Lastly, clear screen.
+ * 
+ * ***Use the search! You will need:
+ * 
+ * -forever block 
+ * 
+ * -while true/do loop block
+ * 
+ * - ___=___ logic block (change it to the correct comparison symbol)
+ * 
+ * -ROUND sound level block
+ * 
+ * -clear screen block
+ */
